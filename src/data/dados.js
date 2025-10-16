c2e04b24f7ecfcd79f664db708b69e5b87ba1d8a
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-10-16 08:56:59 -0300

    criando o arquivo de dados para ser consumido

---
+++ src/data/dados.js
@@ -0,0 +1,52 @@
+const menu=[
+    {
+        id:1,
+        titulo:"Jogo-1",
+        categoria:"gamer",
+        preco:200.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 1"
+    },
+     {
+        id:2,
+        titulo:"Jogo-2",
+        categoria:"acao",
+        preco:100.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 2"
+    },
+      {
+        id:3,
+        titulo:"Jogo-3",
+        categoria:"rpg",
+        preco:150.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 3"
+    },
+      {
+        id:4,
+        titulo:"Jogo-4",
+        categoria:"aventura",
+        preco:100.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 4"
+    },
+      {
+        id:5,
+        titulo:"Jogo-5",
+        categoria:"acao",
+        preco:500.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 5"
+    },
+
+      {
+        id:6,
+        titulo:"Jogo-6",
+        categoria:"aventura",
+        preco:120.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo top 6"
+    },
+]
+export default menu
